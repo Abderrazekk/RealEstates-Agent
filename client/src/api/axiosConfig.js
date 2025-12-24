350c322d8d245e10a0336d43d271154c67fef278
Abderazek <abderazekbelhadjmbarek@gmail.com>
2025-12-24 23:32:06 +0100

    Update all files

--- client/src/api/axiosConfig.js
+++ client/src/api/axiosConfig.js
@@ -34,18 +34,3 @@ apiClient.interceptors.response.use(
 );
 
 export default apiClient;
-
-// Usage example:
-// import apiClient from './axiosConfig';
-// apiClient.get('/endpoint').then(response => console.log(response.data));
-// apiClient.post('/endpoint', data).then(response => console.log(response.data));
-// apiClient.put('/endpoint', data).then(response => console.log(response.data));
-// apiClient.delete('/endpoint').then(response => console.log(response.data));
-// apiClient.patch('/endpoint', data).then(response => console.log(response.data));
-// apiClient.head('/endpoint').then(response => console.log(response.data));
-// apiClient.options('/endpoint').then(response => console.log(response.data));
-// apiClient.request({ method: 'get', url: '/endpoint' }).then(response => console.log(response.data));
-// apiClient.all([apiClient.get('/endpoint1'), apiClient.get('/endpoint2')])
-//   .then(axios.spread((res1, res2) => {
-//     console.log(res1.data, res2.data);
-//   }));
